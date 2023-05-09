7de78343183ba24e2da517c5bd362442ddcf5743
Isuru Gayan <isuru@pop-os.localdomain>
2023-05-09 16:55:15 +0530

    chore : changes for best practices

--- app.py
+++ app.py
@@ -23,14 +23,14 @@ app = Flask(__name__)
 CORS(app)
 
 # Set a secret key for your Flask app
-app.config['SECRET_KEY'] = config('SECRET_KEY')
+app.config["SECRET_KEY"] = config("SECRET_KEY")
 
 # Database configuration
-MYSQL_HOST = config('MYSQL_HOST')
-MYSQL_USER = config('MYSQL_USER')
-MYSQL_PASSWORD = config('MYSQL_PASSWORD')
-MYSQL_DB = config('MYSQL_DB')
-MYSQL_PORT = config('MYSQL_PORT', cast=int)
+MYSQL_HOST = config("MYSQL_HOST")
+MYSQL_USER = config("MYSQL_USER")
+MYSQL_PASSWORD = config("MYSQL_PASSWORD")
+MYSQL_DB = config("MYSQL_DB")
+MYSQL_PORT = config("MYSQL_PORT", cast=int)
 
 
 # Function to create database connection pool
@@ -50,11 +50,11 @@ async def create_pool():
 def token_required(f):
     @wraps(f)
     async def decorated_function(*args, **kwargs):
-        token = request.headers.get('Authorization')
-        if token is None:
+        token = request.headers.get("Authorization")
+        if not token:
             return jsonify({"error": "Token is missing"}), 401
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            data = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
             current_user = await check_user_exist(id=data["user_id"])
         except:
             return jsonify({"error": "Token is invalid"}), 401
@@ -64,7 +64,7 @@ def token_required(f):
 
 
 # Get All Users
-@app.get('/users')
+@app.get("/users")
 async def get_users():
     pool = await create_pool()
     async with pool.acquire() as conn:
@@ -73,33 +73,33 @@ async def get_users():
             users = await cur.fetchall()
             await cur.close()
             pool.close()
-            users_dict_list = [dict(zip(('user_id', 'user_name', 'email', 'password'), row)) for row in users]
-            return {'users': users_dict_list}, 200
+            users_dict_list = [dict(zip(("user_id", "user_name", "email", "password"), row)) for row in users]
+            return {"users": users_dict_list}, 200
 
 
 # Get User using Id
-@app.get('/users/<int:user_id>')
+@app.get("/users/<int:user_id>")
 async def get_user(user_id):
     user = await check_user_exist(id=user_id)
     if user:
-        user_dict = dict(zip(('user_id', 'user_name', 'email'), user))
-        return {'user': user_dict}, 200
+        user_dict = dict(zip(("user_id", "user_name", "email"), user))
+        return {"user": user_dict}, 200
     else:
         return jsonify({"Error": "User not exists"}), 404
 
 
 # Add User
-@app.post('/users')
+@app.post("/users")
 async def add_user():
     if request.is_json:
         user = request.get_json()
-        required_fields = ['user_name', 'email', 'password']
+        required_fields = ["user_name", "email", "password"]
         for field in required_fields:
             if field not in user:
                 return {"error": f"{field} field is required"}, 400
             elif not user.get(field):
                 return {"error": f"{field} field is empty"}, 400
-        if await check_user_exist(name=user["user_name"]) is not None:
+        if await check_user_exist(name=user["user_name"]):
             return {"error": "User already exists"}, 400
         pool = await create_pool()
         async with pool.acquire() as conn:
@@ -110,7 +110,7 @@ async def add_user():
                 last_insert_id = cur.lastrowid
                 await cur.close()
                 pool.close()
-                return jsonify({'message': 'User created successfully!', 'last_insert_id': last_insert_id}), 201
+                return jsonify({"message": "User created successfully!", "last_insert_id": last_insert_id}), 201
     return {"error": "Request must be JSON"}, 415
 
 
@@ -119,7 +119,7 @@ async def check_user_exist(id=None, name=None):
     pool = await create_pool()
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
-            if id is None:
+            if not id:
                 await cur.execute("SELECT * FROM users WHERE user_name = %s", (name,))
             else:
                 await cur.execute("SELECT user_id, user_name, email FROM users WHERE user_id = %s", (id,))
@@ -130,14 +130,14 @@ async def check_user_exist(id=None, name=None):
 
 
 # Update User
-@app.put('/users/<int:user_id>')
+@app.put("/users/<int:user_id>")
 async def update_user(user_id):
-    if await check_user_exist(id=user_id) is None:
+    if not await check_user_exist(id=user_id):
         return {"error": "User not found"}, 404
     else:
         if request.is_json:
             update_user = request.get_json()
-            required_fields = ['user_name', 'email', 'password']
+            required_fields = ["user_name", "email", "password"]
             for field in required_fields:
                 if field not in update_user:
                     return {"error": f"{field} field is required"}, 400
@@ -148,7 +148,7 @@ async def update_user(user_id):
                 async with pool.acquire() as conn:
                     async with conn.cursor() as cur:
                         await cur.execute("UPDATE users SET user_name=%s, email=%s, password=%s WHERE user_id=%s",
-                                          (update_user['user_name'], update_user['email'], update_user['password'],
+                                          (update_user["user_name"], update_user["email"], update_user["password"],
                                            user_id))
                         await cur.close()
                         pool.close()
@@ -158,9 +158,9 @@ async def update_user(user_id):
 
 
 # Delete User
-@app.delete('/users/<int:user_id>')
+@app.delete("/users/<int:user_id>")
 async def delete_user(user_id):
-    if await check_user_exist(id=user_id) is None:
+    if not await check_user_exist(id=user_id):
         return {"error": "User not found"}, 404
     else:
         pool = await create_pool()
@@ -169,77 +169,77 @@ async def delete_user(user_id):
                 await cur.execute("DELETE FROM users WHERE user_id = %s", (user_id,))
                 await cur.close()
                 pool.close()
-                return jsonify({'message': 'User deleted successfully!'}), 200
+                return jsonify({"message": "User deleted successfully!"}), 200
 
 
 # Obtain token access and refresh pairs endpoint
-@app.post('/login')
+@app.post("/login")
 async def login():
-    user_name = request.json['user_name']
-    password = request.json['password']
+    user_name = request.json["user_name"]
+    password = request.json["password"]
     user = await check_user_exist(name=user_name)
-    user_dict = dict(zip(('user_id', 'user_name', 'email', 'password'), user))
-    if not user or not check_password_hash(user_dict['password'], password):
-        return jsonify({'error': 'Invalid username or password.'}), 401
-    return create_and_return_token(user_dict['user_id'])
+    user_dict = dict(zip(("user_id", "user_name", "email", "password"), user))
+    if not user or not check_password_hash(user_dict["password"], password):
+        return jsonify({"error": "Invalid username or password."}), 401
+    return create_and_return_token(user_dict["user_id"])
 
 
 # Obtain new access token from refresh token endpoint
-@app.route('/refresh', methods=['POST'])
+@app.post("/refresh")
 async def refresh():
-    refresh_token = request.json['refresh_token']
+    refresh_token = request.json["refresh_token"]
     try:
-        token_payload = jwt.decode(refresh_token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        token_payload = jwt.decode(refresh_token, app.config["SECRET_KEY"], algorithms=["HS256"])
     except jwt.ExpiredSignatureError:
-        return jsonify({'error': 'Refresh token has expired.'}), 401
+        return jsonify({"error": "Refresh token has expired."}), 401
     except jwt.InvalidTokenError:
-        return jsonify({'error': 'Invalid refresh token.'}), 401
-    return create_and_return_token(token_payload['user_id'])
+        return jsonify({"error": "Invalid refresh token."}), 401
+    return create_and_return_token(token_payload["user_id"])
 
 
 def create_and_return_token(user_id):
     access_expire = int(time.time()) + 3600
     refresh_expire = int(time.time()) + 604800
-    access_token = jwt.encode({'user_id': user_id, 'exp': access_expire},
-                              app.config['SECRET_KEY'],
-                              algorithm='HS256')
-    refresh_token = jwt.encode({'user_id': user_id, 'exp': refresh_expire},
-                               app.config['SECRET_KEY'], algorithm='HS256')
-    return jsonify({'access_token': access_token, 'refresh_token': refresh_token,
-                    'access_expire': datetime.fromtimestamp(access_expire),
-                    'refresh_expire': datetime.fromtimestamp(refresh_expire)}), 200
+    access_token = jwt.encode({"user_id": user_id, "exp": access_expire},
+                              app.config["SECRET_KEY"],
+                              algorithm="HS256")
+    refresh_token = jwt.encode({"user_id": user_id, "exp": refresh_expire},
+                               app.config["SECRET_KEY"], algorithm="HS256")
+    return jsonify({"access_token": access_token, "refresh_token": refresh_token,
+                    "access_expire": datetime.fromtimestamp(access_expire),
+                    "refresh_expire": datetime.fromtimestamp(refresh_expire)}), 200
 
 
 # Add pin
-@app.post('/pins')
+@app.post("/pins")
 @token_required
 async def add_pin(current_user):
-    required_fields = {'title': request.form.get('title'), 'body': request.form.get('body'),
-                       'image': request.files.get('image')}
+    required_fields = {"title": request.form.get("title"), "body": request.form.get("body"),
+                       "image": request.files.get("image")}
     for field, value in required_fields.items():
-        if value is None or value == '':
-            return jsonify({'error': f'{field} is required.'}), 400
-    filename = secure_filename(required_fields['image'].filename)
-    required_fields['image'].save(filename)
+        if not value:
+            return jsonify({"error": f"{field} is required."}), 400
+    filename = secure_filename(required_fields["image"].filename)
+    required_fields["image"].save(filename)
     image = filename
     added_date = date.today().strftime("%Y-%m-%d")
     pool = await create_pool()
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
             await cur.execute("INSERT INTO pins (title, body, image, user_id, added_date) VALUES (%s, %s, %s, %s, %s)",
-                              (required_fields['title'], required_fields['body'], image, current_user[0], added_date))
+                              (required_fields["title"], required_fields["body"], image, current_user[0], added_date))
             last_insert_id = cur.lastrowid
             await cur.close()
             pool.close()
-            return jsonify({'message': 'Pin created successfully!', 'last_insert_id': last_insert_id}), 201
+            return jsonify({"message": "Pin created successfully!", "last_insert_id": last_insert_id}), 201
 
 
 # Get All pins
-@app.get('/pins')
+@app.get("/pins")
 async def get_pins():
-    created_by = request.args.get('created_by')
-    order_by_field = request.args.get('order_by_field')
-    order_by = request.args.get('order_by')
+    created_by = request.args.get("created_by")
+    order_by_field = request.args.get("order_by_field")
+    order_by = request.args.get("order_by")
     if created_by:
         user = await check_user_exist(name=created_by)
         user_id = user[0]
@@ -257,19 +257,19 @@ async def get_pins():
             pins = await cur.fetchall()
             await cur.close()
             pool.close()
-            pins_dict_list = [dict(zip(('pin_id', 'title', 'body', 'image', 'user_id', 'added_date'), row)) for row in
+            pins_dict_list = [dict(zip(("pin_id", "title", "body", "image", "user_id", "added_date"), row)) for row in
                               pins]
-            return jsonify({'pins': pins_dict_list}), 200
+            return jsonify({"pins": pins_dict_list}), 200
 
 
 # Get Pin By ID
-@app.get('/pins/<int:pin_id>')
+@app.get("/pins/<int:pin_id>")
 async def get_pin(pin_id):
     pin = await check_pin_exists(pin_id=pin_id)
     if not pin:
-        return jsonify({'error': 'Pin does not exist'}), 404
-    pin_dict = dict(zip(('pin_id', 'title', 'body', 'image', 'user_id', 'added_date'), pin))
-    return jsonify({'pin': pin_dict}), 200
+        return jsonify({"error": "Pin does not exist"}), 404
+    pin_dict = dict(zip(("pin_id", "title", "body", "image", "user_id", "added_date"), pin))
+    return jsonify({"pin": pin_dict}), 200
 
 
 # Check if pin exists and return pin
@@ -277,7 +277,7 @@ async def check_pin_exists(user_id=None, pin_id=None):
     pool = await create_pool()
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
-            if user_id is None:
+            if not user_id:
                 await cur.execute("SELECT * FROM pins where pin_id =%s", (pin_id))
             else:
                 await cur.execute("SELECT * FROM pins where pin_id =%s and user_id =%s", (pin_id, user_id))
@@ -288,14 +288,14 @@ async def check_pin_exists(user_id=None, pin_id=None):
 
 
 # Update pin
-@app.put('/pins/<int:pin_id>')
+@app.put("/pins/<int:pin_id>")
 @token_required
 async def update_pin(current_user, pin_id):
-    if await check_pin_exists(user_id=current_user[0], pin_id=pin_id) is None:
+    if not await check_pin_exists(user_id=current_user[0], pin_id=pin_id):
         return {"error": "Pin not found"}, 404
-    title = request.form['title']
-    body = request.form['body']
-    f = request.files.get('image')
+    title = request.form["title"]
+    body = request.form["body"]
+    f = request.files.get("image")
     filename = secure_filename(f.filename)
     image = filename
     old_pin = await check_pin_exists(current_user[0], pin_id)
@@ -317,22 +317,22 @@ async def update_pin(current_user, pin_id):
             updated_pin = await check_pin_exists(current_user[0], pin_id)
             await cur.close()
             pool.close()
-            pin_dict = dict(zip(('pin_id', 'title', 'body', 'image', 'user_id', 'added_date'), updated_pin))
-            return jsonify({'message': 'Pin updated successfully!', "Updated Pin": pin_dict}), 200
+            pin_dict = dict(zip(("pin_id", "title", "body", "image", "user_id", "added_date"), updated_pin))
+            return jsonify({"message": "Pin updated successfully!", "Updated Pin": pin_dict}), 200
 
 
 # Delete Pin
-@app.delete('/pins/<int:pin_id>')
+@app.delete("/pins/<int:pin_id>")
 @token_required
 async def delete_pin(current_user, pin_id):
-    if await check_pin_exists(current_user[0], pin_id) is None:
+    if not await check_pin_exists(current_user[0], pin_id):
         return {"error": "Pin not found"}, 404
     pool = await create_pool()
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
             await cur.execute("DELETE FROM pins WHERE pin_id = %s and user_id = %s", (pin_id, current_user[0]))
-            return jsonify({'message': 'Pin deleted successfully!'}), 200
+            return jsonify({"message": "Pin deleted successfully!"}), 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=8001)
